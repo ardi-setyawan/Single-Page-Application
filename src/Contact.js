6fd41f73613bbaa864917ecfa372db789ca96267
ardi setyawan <ardisetyawan1621@gmail.com>
2019-12-06 00:04:40 +0700

    membuat single page application dengan react

---
+++ src/Contact.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+    render(){
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>
+                    Email : Ardisetyawan1621@gmail.com <br/>
+                    Phone : 085701769488
+                </p>
+
+            </div>
+
+        );
+    }
+}
+
+export default Contact;
